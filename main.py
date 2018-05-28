fbe9c2af854f782ccbeca6a3e8a079bab2296fc3
Luiz Garcia <38627084+luizfmgarcia@users.noreply.github.com>
2018-05-28 10:41:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Create the first generation of solutions
+def start:
+
+# Separation of solutions into 2 populations
+def 2pop:
+
+# Detect the violation of a restriction into a solution
+def in_feasible:
+    
+# Calculate de Fitness of the solution
+def calc_fit:
+
+# Make a random selection into the solutions
+def selection:
+
+# Generate new solutions from the actual population
+def new_generation:
+
+# Make a mutation into a solution
+def mutation:
+
+# Make a crossover between two solutions    
+def crossover:
+
+# Detect the stop condition
+def stop:
+    
+def main:
+    
