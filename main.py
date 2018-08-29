84b256106393eee660e4f956d7e6f577be88148f
Luiz Garcia <38627084+luizfmgarcia@users.noreply.github.com>
2018-08-28 22:54:02 -0300

    0.0

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 # Keep the details of a professor
 class Prof:
-        def __init__(self, name, local, research, charge):
-                self._name = name
-                self._local = local
-                #self._research = research
-                self._charge = charge
+    def __init__(self, name, local, research, charge):
+        self._name = name
+        self._local = local
+        #self._research = research
+        self._charge = charge
 
 # Keep the details of a subject
 class Subject:
-        def __init__(self, daysHours, local, charge):
-                self._daysHours = name
-                self._local = local
-                self._charge = charge
+    def __init__(self, daysHours, local, charge):
+        self._daysHours = name
+        self._local = local
+        self._charge = charge
 
 # Keep the details of a Candidate
 class Candidate:
-        def __init__(self):
-                self._list = list()
-                self._relation = list()
+    def __init__(self):
+        self._list = list()
+        self._relation = list()
 
-        def __insert__(self, Prof, Subject):
-                self._relation.add(Prof, Subject)
-                self._list.add(self._relation)
+    def __insert__(self, Prof, Subject):
+        self._relation.add(Prof, Subject)
+        self._list.add(self._relation)
                 
 # Keep all Candidates obtained during a run of the algorithm
 class Solutions:
-        def __init__(self):
-                self._listCandidates = list()
-                self._pop = None
+    def __init__(self):
+        self._listCandidates = list()
+        self._pop = None
+    
+    def __feas__(self):
+        self._pop = 'f'
 
-        def __feas__(self):
-                self._pop = 'f'
-
-        def __infeas__(self):
-                self._pop = 'i'
+    def __infeas__(self):
+        self._pop = 'i'
                 
 ### Assign the type Feasible for a solution and gives an especific function
 ##class Feasible(solution):
@@ -45,35 +45,45 @@ class Solutions:
 ##        def __init__(self):
 ##                self._pop = "i"
 
-
-# Create the first generation of solutions
-def start:
-
-# Separation of solutions into 2 populations
-def 2pop:
-
-# Detect the violation of a restriction into a solution
-def in_feasible:
+class UCTP:
+    # Create the first generation of solutions
+    def start():
+        pass
     
-# Calculate de Fitness of the solution
-def calc_fit:
-
-# Make a random selection into the solutions
-def selection:
-
-# Generate new solutions from the actual population
-def new_generation:
-
-# Make a mutation into a solution
-def mutation:
-
-# Make a crossover between two solutions    
-def crossover:
-
-# Detect the stop condition
-def stop:
+    # Separation of solutions into 2 populations
+    def two_pop():
+        pass
     
-# main
-from random import Random
-rand = Random() 
-
+    # Detect the violation of a restriction into a solution
+    def in_feasible():
+        pass
+    
+    # Calculate de Fitness of the solution
+    def calc_fit():
+        pass
+    
+    # Make a random selection into the solutions
+    def selection():
+        pass
+    
+    # Generate new solutions from the actual population
+    def new_generation():
+        pass
+        
+    # Make a mutation into a solution
+    def mutation():
+        pass
+    
+    # Make a crossover between two solutions    
+    def crossover():
+        pass
+    
+    # Detect the stop condition
+    def stop():
+        pass
+    
+    # main
+    def main():
+        from random import Random
+        rand = Random() 
+    
