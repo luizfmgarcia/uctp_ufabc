20d83cba187eb41cb548881886aa70f52dec8c1e
Luiz Garcia <38627084+luizfmgarcia@users.noreply.github.com>
2018-09-19 20:43:45 -0300

    0.0

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 # Keep the details of a professor
 class Prof:
     def __init__(self, name, local, research, charge):
-        self._name = name
-        self._local = local
+        self.name = name
+        self.local = local
         #self._research = research
-        self._charge = charge
+        self.charge = charge
 
 # Keep the details of a subject
 class Subject:
     def __init__(self, daysHours, local, charge):
-        self._daysHours = name
-        self._local = local
-        self._charge = charge
+        self.daysHours = daysHours
+        self.local = local
+        self.charge = charge
 
 # Keep the details of a Candidate
 class Candidate:
     def __init__(self):
-        self._list = list()
-        self._relation = list()
+        self.candidate = []
+        self.relation = []
 
     def __insert__(self, Prof, Subject):
-        self._relation.add(Prof, Subject)
-        self._list.add(self._relation)
+        self.relation.add(Prof, Subject)
+        self.candidate.add(self._relation)
                 
 # Keep all Candidates obtained during a run of the algorithm
 class Solutions:
     def __init__(self):
-        self._listCandidates = list()
-        self._pop = None
+        self.listCandidates = []
+        self.pop = None
     
     def __feas__(self):
         self._pop = 'f'
 
     def __infeas__(self):
         self._pop = 'i'
+    
+    def __resetPop__(self):
+        self._pop = None
                 
 ### Assign the type Feasible for a solution and gives an especific function
 ##class Feasible(solution):
@@ -48,7 +51,8 @@ class Solutions:
 class UCTP:
     # Create the first generation of solutions
     def start():
-        pass
+        # Read the data of professors and subjects
+        return
     
     # Separation of solutions into 2 populations
     def two_pop():
@@ -58,7 +62,7 @@ class UCTP:
     def in_feasible():
         pass
     
-    # Calculate de Fitness of the solution
+    # Calculate the Fitness of the solution
     def calc_fit():
         pass
     
@@ -84,6 +88,18 @@ class UCTP:
     
     # main
     def main():
+        # Random operator
         from random import Random
-        rand = Random() 
+        rand = Random()
+        # Count of iterations of work
+        t = 0;
+        # Base List of Professors and Subjects
+        prof = list()
+        subj = list()
+        # Start of the works
+        self.start()
+        # Main work - iterations to find a solution
+        while(t!=100):
+             t = t+1
+        print("FIM")
     
